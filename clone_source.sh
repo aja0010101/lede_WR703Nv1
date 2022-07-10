e6af31fa3621c39d87e141971cc42084c1c047cc
aja0010101 <61653782+aja0010101@users.noreply.github.com>
2022-07-10 10:06:07 +0800

    Update clone_source.sh

--- clone_source.sh
+++ clone_source.sh
@@ -9,8 +9,8 @@ sed -i -e 's/192.168.1.1/192.168.123.1/g' -e 's/OpenWrt/TPLink_WR703Nv1/g' packa
 # sed -i '343s/4/16/g' target/linux/ar71xx/image/tiny-tp-link.mk  #修改wr703nv1的编译固件大小
 rm -rf target/linux/ath79/image/tiny-tp-link.mk
 rm -rf target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
-wget https://am.sohaha.xyz/tp703/tiny-tp-link.mk -O target/linux/ath79/image/tiny-tp-link.mk
-wget https://am.sohaha.xyz/tp703/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi -O target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
+# wget https://somewhere.com/tp703/tiny-tp-link.mk -O target/linux/ath79/image/tiny-tp-link.mk
+# wget https://somewhere.com/tp703/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi -O target/linux/ath79/dts/ar9331_tplink_tl-wr703n_tl-mr10u.dtsi
 sed -i -e 's/OpenWrt/TPLink_WR703Nv1/g' -e 's/encryption=none/encryption=psk2/g' -e '/psk2/a\			set wireless.default_radio${devidx}.key=password' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 # 修改WiFi名称,添加加密方式和密码
 
